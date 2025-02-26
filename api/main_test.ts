c97894573060aeec45883eef58ec55e321650c27
molmos <hello@molmos.dev>
2025-02-26 22:17:11 +0100

    Remove main_test.ts as it is no longer needed

--- api/main_test.ts
+++
@@ -1,7 +0,0 @@
-import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
-import app from "./main.ts";
-
-Deno.test("GET / should return 'Hello World!'", async () => {
-  const request = await superoak(app);
-  await request.get("/").expect(200).expect("Hello World!");
-});
