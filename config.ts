7435c8873b17b22e2f664d5d7f2f758d55580dff
molmos <hello@molmos.dev>
2025-02-26 22:16:01 +0100

    Refactor configuration to use Deno.env for environment variables

--- config.ts
+++ config.ts
@@ -1,13 +1,13 @@
-import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
-
-const env = config();
 
 export default {
   production: false,
   swagger: {
-    user: env.SWAGGER_USER,
-    password: env.SWAGGER_PASSWORD,
+    user: Deno.env.get("SWAGGER_USER"),
+    password: Deno.env.get("SWAGGER_PASSWORD"),
   },
-  supabase: createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY),
+  supabase: createClient(
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+    Deno.env.get("SUPABASE_KEY")!
+  ),
 };
