a4b0d17bf1e10f49ac2c0273e0f40dbaf5df566f
molmosdev <hello@molmos.dev>
2025-02-28 01:18:58 +0100

    Refactor config to directly use Deno.env.get for environment variable retrieval

--- api/config.ts
+++ api/config.ts
@@ -1,26 +1,13 @@
 import { createClient } from "@supabase";
 
-/**
- * Get environment variable
- * @param key - The key of the environment variable
- * @returns The value of the environment variable
- */
-function getEnvVariable(key: string): string {
-  const value = Deno.env.get(key);
-  if (!value) {
-    throw new Error(`Environment variable ${key} is not defined`);
-  }
-  return value;
-}
-
 export default {
   production: false,
   swagger: {
-    user: getEnvVariable("SWAGGER_USER"),
-    password: getEnvVariable("SWAGGER_PASSWORD"),
+    user: Deno.env.get("SWAGGER_USER"),
+    password: Deno.env.get("SWAGGER_PASSWORD"),
   },
   database: createClient(
-    getEnvVariable("SUPABASE_SERVICE_ROLE_KEY"),
-    getEnvVariable("SUPABASE_KEY")
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+    Deno.env.get("SUPABASE_KEY")!
   ),
 };
