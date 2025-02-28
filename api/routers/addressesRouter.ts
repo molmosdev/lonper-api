d17b2f699e42754e62dc7180d67b5799f5409e9d
molmosdev <hello@molmos.dev>
2025-02-28 14:02:20 +0100

    Add JWT validation middleware to addresses API routes for enhanced security

--- api/routers/addressesRouter.ts
+++ api/routers/addressesRouter.ts
@@ -1,11 +1,16 @@
 import { Hono } from "hono";
 import AddressesController from "@controllers/addressesController.ts";
+import jwtValidation from "@middlewares/jwtValidation.ts";
 
 const router = new Hono();
 
-router.get("/getAddresses", AddressesController.getAddresses);
-router.post("/postAddress", AddressesController.postAddress);
-router.put("/putAddress", AddressesController.putAddress);
-router.delete("/deleteAddress", AddressesController.deleteAddress);
+router.get("/getAddresses", jwtValidation, AddressesController.getAddresses);
+router.post("/postAddress", jwtValidation, AddressesController.postAddress);
+router.put("/putAddress", jwtValidation, AddressesController.putAddress);
+router.delete(
+  "/deleteAddress",
+  jwtValidation,
+  AddressesController.deleteAddress
+);
 
 export default router;
