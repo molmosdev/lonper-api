788290949851b66449091fa7fa75b501f2c9c14d
molmosdev <hello@molmos.dev>
2025-03-01 19:57:46 +0100

    Refactor CORS and Swagger configurations into middleware for improved code organization

--- api/main.ts
+++ api/main.ts
@@ -1,60 +1,15 @@
 import { Hono } from "hono";
-import config from "@config";
-import { cors } from "hono/cors";
-import { openAPISpecs } from "npm:hono-openapi@0.4.5";
-import { swaggerUI } from "@hono/swagger-ui";
-import { basicAuth } from "hono/basic-auth";
+import { corsMiddleware } from "@middlewares/cors.ts";
+import { setupSwagger } from "@middlewares/swagger.ts";
 import rootRouter from "@routers/index.ts";
 
 const app = new Hono();
 
 // CORS configuration
-const allowedOrigins = config.production
-  ? ["https://www.portal.lonper.molmos.dev"]
-  : ["http://localhost:4200"];
-
-app.use(
-  "/api/*",
-  cors({
-    origin: (origin) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        return origin;
-      }
-      return null;
-    },
-    credentials: true,
-  }),
-);
+app.use("/api/*", corsMiddleware());
 
 // Swagger configuration
-app.get("/docs", swaggerUI({ url: "/openapi" }));
-
-app.get(
-  "/openapi",
-  basicAuth({
-    username: config.swagger.user!,
-    password: config.swagger.password!,
-  }),
-  openAPISpecs(app, {
-    documentation: {
-      info: {
-        title: "Portal Lonper API",
-        version: "2.0.0",
-        description: "Portal Lonper API documentation",
-      },
-      servers: [
-        {
-          url: "http://localhost:8000",
-          description: "Local server",
-        },
-        {
-          url: "https://api.lonper.molmos.dev",
-          description: "Production server",
-        },
-      ],
-    },
-  }),
-);
+setupSwagger(app); // Usa la función combinada setupSwagger
 
 // Routers
 app.route("/api/v2", rootRouter);
---
+++ api/middlewares/cors.ts
@@ -0,0 +1,23 @@
+import { cors } from "hono/cors";
+import { MiddlewareHandler } from "hono";
+import config from "@config";
+
+/**
+ * CORS middleware
+ * @returns CORS middleware handler
+ */
+export const corsMiddleware = (): MiddlewareHandler => {
+  const allowedOrigins = config.production
+    ? ["https://www.portal.lonper.molmos.dev"]
+    : ["http://localhost:4200"];
+
+  return cors({
+    origin: (origin) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        return origin;
+      }
+      return null;
+    },
+    credentials: true,
+  });
+};
